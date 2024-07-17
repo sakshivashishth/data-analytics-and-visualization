26bbc012ebab5c307f6e42126e2b5e075aa09b48
sakshivashishth <sakshivashishth56@gmail.com>
2024-07-17 17:14:13 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,4 @@
+#create a list of 20 numbers and display the elements in backward direction using forward index
+list1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for item in list1[::-1]:
+    print(item)
