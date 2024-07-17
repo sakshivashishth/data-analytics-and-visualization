ff121b394bf1cefb616a0ba4625749bc45cebb80
sakshivashishth <sakshivashishth56@gmail.com>
2024-07-17 16:35:27 +0530

    Add files via upload
    to find the greatest number between two numbers

---
+++ greatest.py
@@ -0,0 +1,10 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+if(a>b):
+    print("a is greatest")
+
+elif(b>a):
+    print("b is greatest")
+else:
+    print("both are equal")
+    
